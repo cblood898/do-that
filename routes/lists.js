e47102a201c2b66bd2806209966797028b068e56
emRae <emRae@users.noreply.github.com>
2017-02-25 14:31:07 -0700

    updating routes

--- routes/lists.js
+++ routes/lists.js
@@ -2,27 +2,48 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  List.find({ : req.query.boardId }, ( err, boards ) => {
-    res.json(boards);
+  List.find( ( err, lists ) => {
+    res.json(lists);
   });
 });
 
 router.post('/', (req, res) => {
-  let { boardId, name } = req.body;
   new List({
-    name,
-    boardId
+    name: req.body.name
   }).save( (err, list) => {
     res.json(list);
   });
 });
 
-router.delete('/:id', (req, res) => {
-  List.findById(req.params.id, (err, list) => {
-    list.remove();
-    Card.find({'listId': req.query.id}).remove().exec( (err, list) => {
-      res.status(200).send({success: true});
+router.put('/:id', ( req, res ) => {
+  let { name } = req.body;
+  List.findByIdAndUpdate(
+    req.params.id,
+    { $set: { name }},
+    { new: true },
+    (err, list) => {
+      res.json(list);
     });
+});
+
+router.delete('/:id', (req, res) => {
+  let boardId = req.params.id;
+  List.findById(boardId, (err, list) => {
+    list.remove();
+    // List.find({ boardId }, (err, lists) => {
+    //   lists.forEach( list => { 
+    //     Card.find({'listId': list._id}).remove().exec();
+    //     list.remove() 
+    //   } );
+    // });
+    res.status(200).send({success: true});
   });
 });
+
+router.get('/:id', (req, res) => {
+  List.findById(req.params.id, (err, list) => {
+    res.render('list', { list })
+  })
+});
+
 module.exports = router;
